7adb16d9d5c51648e2ee56cf7e0a57ec77565bec
Rik Tonnaer <riktonnaer@hotmail.com>
2022-08-21 22:03:08 +0200

    added all other conversions

--- intergas_pc_interface.py
+++ intergas_pc_interface.py
@@ -89,7 +89,34 @@ class intergas_pc_interface:
     self.opentherm = flags.b.opentherm
     
     # Conversion of Flags in Byte 29
-
+    B29flags = B29Flags()
+    B29flags.asbyte = self.recv[29]
+    self.gasvalve = B29flags.b.gasvalve
+    self.spark =  B29flags.b.spark
+    self.io_signal = B29flags.b.io_signal
+    self.ch_ot_disabled = B29flags.b.ch_ot_disabled
+    self.low_water_pressure = B29flags.b.low_water_pressure
+    self.pressure_sensor = B29flags.b.pressure_sensor
+    self.burner_block = B29flags.b.burner_block
+    self.grad_flag = B29flags.b.grad_flag
+  
+    # Conversion of the CH Pressure ?? 
+    self.ch_pressure = None
+    if not B29flags.b.pressure_sensor:
+      self.ch_pressure = -35
+    # Getting the Display Code  
+    self.displ_code = self.recv[24]
+    # conversion of display code to textstring
+    c2s = {
+      51: "Warm water",
+      102: "CV Brandt",
+      126: "CV in rust",
+      204: "Tapwater nadraaien",
+      231: "CV nadraaien",
+    }
+  
+  self.status = c2s.get(self.displ_code, "Onbekend (%s)" % self.displ_code)
+  
 
   def connect(self):  
     ''' method for connecting to the given serial port '''
@@ -146,10 +173,17 @@ class intergas_pc_interface:
     tableFlags.add_row("Cascade Klep?",self.ch_cascade_relay)
     tableFlags.add_row("Open Therm",self.opentherm)
 
+    tableMisc = Table(title="Intergas Data - Misc")
+    tableMisc.add_column('Description')
+    tableMisc.add_column('Value')
+    
+    tableMisc.add_row("STatus",self.status)
+
     # Rendering the Console
     console = Console()
     console.print(table)
     console.print(tableFlags)
+    console.print(tableMisc)
 
 if __name__ == '__main__':
   # initate the connection to the serial interface at port  
